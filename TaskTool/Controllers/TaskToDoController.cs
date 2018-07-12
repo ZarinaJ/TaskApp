e94e39c78429618754a93ca5b3bd27c9bf3950b8
Zarina <zzhanabilova@intarget.kz>
2018-07-12 14:12:45 +0600

    Add project files.

---
+++ TaskTool/Controllers/TaskToDoController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TaskTool.Models;
+
+namespace TaskTool.Controllers
+{
+    public class TaskToDoController : Controller
+    {
+        private TaskDbEntities1 db = new TaskDbEntities1();
+
+        // GET: TaskToDo
+        public ActionResult Index()
+        {
+            return View(db.Tasks.ToList());
+        }
+
+        // GET: TaskToDo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tasks tasks = db.Tasks.Find(id);
+            if (tasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tasks);
+        }
+
+        // GET: TaskToDo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TaskToDo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Status")] Tasks tasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(tasks);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else { }
+
+            return View(tasks);
+        }
+
+        // GET: TaskToDo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tasks tasks = db.Tasks.Find(id);
+            if (tasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tasks);
+        }
+
+        // POST: TaskToDo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Status")] Tasks tasks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tasks).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tasks);
+        }
+
+        // GET: TaskToDo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tasks tasks = db.Tasks.Find(id);
+            if (tasks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tasks);
+        }
+
+        // POST: TaskToDo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tasks tasks = db.Tasks.Find(id);
+            db.Tasks.Remove(tasks);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CompletedList()
+        {
+            return View(db.Tasks.ToList().Where(t=>t.Status));
+        }
+        public ActionResult UncompletedList()
+        {
+            return View(db.Tasks.ToList().Where(t => !t.Status));
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public int getNextId()
+        {
+            var nextId = db.Tasks.OrderBy(x => x.Id).Last().Id + 1;
+            return nextId;
+        }
+    }
+}
